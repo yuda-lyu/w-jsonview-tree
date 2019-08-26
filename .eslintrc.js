4d16280aa68e8567f2d1c531bead9242b2f65cdd
yuda-lyu <firsemisphere@gmail.com>
2019-08-26 22:10:35 +0800

    update: pks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,19 @@ module.exports = {
         "es6": true,
         "mocha": true
     },
-    "extends": "standard",
+    "extends": ["plugin:vue/essential", "standard"],
+    "plugins": [
+        "vue"
+    ],
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
         "ecmaVersion": 2018,
-        "sourceType": "module"
+        "sourceType": "module",
     },
-rules: {
+    rules: {
         // allow async-await
         'generator-star-spacing': 'off',
         // allow debugger during development
@@ -37,5 +40,10 @@ rules: {
         'no-constant-condition': 'off',
         'camelcase': 'off',
         'no-new': 'off',
+        'prefer-const': 'off',
+        'quote-props': ['error', 'consistent'],
+        'dot-notation': 'off',
+        'standard/no-callback-literal': 'off',
+        'quotes': ['error', "single", { "allowTemplateLiterals": true }],
     }
 };
